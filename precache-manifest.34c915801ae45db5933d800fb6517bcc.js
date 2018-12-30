3cb6e66b0a43593db90d8edf1e90e657a65dc024
Sally Guo <siqig@andrew.cmu.edu>
2018-12-30 01:06:24 -0500

    Updates

---
+++ precache-manifest.34c915801ae45db5933d800fb6517bcc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "7dd79a8355b07af572803f1fdaac6bb9",
+    "url": "/static/media/resume.7dd79a83.pdf"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "138d63985c0cb4d9c67e",
+    "url": "/static/js/main.138d6398.chunk.js"
+  },
+  {
+    "revision": "2cd8beb6d22a05a47e31",
+    "url": "/static/js/1.2cd8beb6.chunk.js"
+  },
+  {
+    "revision": "138d63985c0cb4d9c67e",
+    "url": "/static/css/main.900d40b7.chunk.css"
+  },
+  {
+    "revision": "9292a7c49659dc3f2e226895c9c2b01d",
+    "url": "/index.html"
+  }
+];
